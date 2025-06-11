7aefb79e0eaceb0c780384eb66db4f31468c30ef
Shanna Noe <snoe.prsvr@gmail.com>
2025-06-11 09:06:14 -0400

    tailwind installed

---
+++ client/vite-project/src/App.tsx
@@ -0,0 +1,15 @@
+
+function App() {
+ 
+  
+  return (
+    <div className="flex h-screen items-center justify-center bg-pink-800">
+      <h1 className="text-4xl font-bold text-red">Hello, Tailwind + TypeScript!</h1>
+      <h1>Shanna</h1>
+    </div>
+    
+    
+  );
+}
+  
+export default App
